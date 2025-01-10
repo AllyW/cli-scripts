910d2e16b770bf817e9fae3bd8b1668597857582
AllyW <liwang1025@gmail.com>
2025-01-10 15:47:32 +0800

    add comvault

---
+++ green-field.bat
@@ -0,0 +1,12 @@
+python .\aaz-dev\diff-aaz-model-from-swagger-tsp.py --rpas ..\swagger-res\aaz\Commands\device-registry --rpat ..\tsp-res\aaz\Commands\device-registry
+python .\aaz-dev\diff-aaz-model-from-swagger-tsp.py --rpas ..\swagger-res\aaz\Commands\compute-schedule --rpat ..\tsp-res\aaz\Commands\compute-schedule
+python .\aaz-dev\diff-aaz-model-from-swagger-tsp.py --rpas ..\swagger-res\aaz\Commands\document-db --rpat ..\tsp-res\aaz\Commands\document-db
+python .\aaz-dev\diff-aaz-model-from-swagger-tsp.py --rpas ..\swagger-res\aaz\Commands\health-data-ai-services --rpat ..\tsp-res\aaz\Commands\health-data-ai-services
+
+python .\aaz-dev\diff-aaz-model-from-swagger-tsp.py --rpas ..\swagger-res\aaz\Commands\compute-fleet --rpat ..\tsp-res\aaz\Commands\compute-fleet
+python .\aaz-dev\diff-aaz-model-from-swagger-tsp.py --rpas ..\swagger-res\aaz\Commands\dev-ops-infrastructure --rpat ..\tsp-res\aaz\Commands\dev-ops-infrastructure
+python .\aaz-dev\diff-aaz-model-from-swagger-tsp.py --rpas ..\swagger-res\aaz\Commands\azure-playwright-service --rpat ..\tsp-res\aaz\Commands\azure-playwright-service
+python .\aaz-dev\diff-aaz-model-from-swagger-tsp.py --rpas ..\swagger-res\aaz\Commands\large-storage-instance --rpat ..\tsp-res\aaz\Commands\large-storage-instance
+
+
+
